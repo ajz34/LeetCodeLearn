820c2b038102363e7221d1366dbfddf6cf2e79bf
ajz34 <17110220038@fudan.edu.cn>
2020-02-10 18:25:28 +0800

    cpp 0003

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -2,18 +2,13 @@
 // Created by ajz34 on 2020/2/9.
 //
 
-#include "e0001_e0100/e0002_add_two_numbers.h"
-#include "main.h"
+#include "e0001_e0100/e0003_longest_string.h"
 
-#include <vector>
+#include <string>
+#include <iostream>
 using namespace std;
 
 int main() {
-    auto l1 = new ListNode(2);
-    l1->next = new ListNode(4);
-    l1->next->next = new ListNode(3);
-    auto l2 = new ListNode(5);
-    l2->next = new ListNode(6);
-    l2->next->next = new ListNode(4);
-    Solution().addTwoNumbers(l1, l2);
+    string s = "bbbbb";
+    cout << Solution().lengthOfLongestSubstring(s) << endl;
 }
