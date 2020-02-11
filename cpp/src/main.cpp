4af641af32f03db9379bd536818422db28045544
ajz34 <17110220038@fudan.edu.cn>
2020-02-11 20:55:51 +0800

    cpp 0014

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -2,12 +2,14 @@
 // Created by ajz34 on 2020/2/9.
 //
 
-#include "e0001_e0100/e0013_roman_to_int.h"
+#include "e0001_e0100/e0014_longest_common_prefix.h"
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    cout << Solution().romanToInt("XXVII") << endl;
-    cout << Solution().romanToInt("MCMXCIV") << endl;
+    vector<string> v { "flower", "flow", "flight" };
+    cout << Solution().longestCommonPrefix(v) << endl;
+    vector<string> l { "dog", "racecar", "car" };
+    cout << Solution().longestCommonPrefix(l) << endl;
 }
