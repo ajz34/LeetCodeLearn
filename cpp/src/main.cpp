9c5eeada739d860e2d08faac7894b267e39f19fb
ajz34 <17110220038@fudan.edu.cn>
2020-02-11 13:18:14 +0800

    cpp 0008

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -2,14 +2,19 @@
 // Created by ajz34 on 2020/2/9.
 //
 
-#include "e0001_e0100/e0007_reverse_integer.h"
+#include "e0001_e0100/e0008_string_to_integer.h"
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    cout << Solution().reverse(-123) << endl;
-    cout << Solution().reverse(-1020) << endl;
-    cout << Solution().reverse(1534236469) << endl;
-    cout << Solution().reverse(-2147483648) << endl;
+    cout << Solution().myAtoi("42") << endl;
+    cout << Solution().myAtoi("   -42") << endl;
+    cout << Solution().myAtoi("4193 with words") << endl;
+    cout << Solution().myAtoi("words and 987") << endl;
+    cout << Solution().myAtoi("-91283472332") << endl;
+    cout << Solution().myAtoi("  0000000000012345678") << endl;
+    cout << Solution().myAtoi("000000000000000000") << endl;
+    cout << Solution().myAtoi("+") << endl;
+    cout << Solution().myAtoi("20000000000000000000") << endl;
 }
