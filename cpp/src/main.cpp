c0e54b80421042e07ae4d04d70848d52b858e290
ajz34 <17110220038@fudan.edu.cn>
2020-02-11 23:05:31 +0800

    cpp 0015

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -2,14 +2,13 @@
 // Created by ajz34 on 2020/2/9.
 //
 
-#include "e0001_e0100/e0014_longest_common_prefix.h"
+#include "e0001_e0100/e0015_three_sum.h"
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    vector<string> v { "flower", "flow", "flight" };
-    cout << Solution().longestCommonPrefix(v) << endl;
-    vector<string> l { "dog", "racecar", "car" };
-    cout << Solution().longestCommonPrefix(l) << endl;
+    vector<int> v { -1, 0, 1, 2, -1, -4 };
+    auto r = Solution().threeSum(v);
+    cout << endl;
 }
