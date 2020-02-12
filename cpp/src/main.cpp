d9117aef8535eab62f33e4cf9fe8b447e698528b
ajz34 <17110220038@fudan.edu.cn>
2020-02-12 10:05:08 +0800

    cpp 0016

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -2,13 +2,12 @@
 // Created by ajz34 on 2020/2/9.
 //
 
-#include "e0001_e0100/e0015_three_sum.h"
+#include "e0001_e0100/e0016_three_sum_closest.h"
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    vector<int> v { -1, 0, 1, 2, -1, -4 };
-    auto r = Solution().threeSum(v);
-    cout << endl;
+    vector<int> v { -1, 2, 1, -4 };
+    cout << Solution().threeSumClosest(v, 1) << endl;
 }
