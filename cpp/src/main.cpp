9fdaffbfd03f3f5bd2923800019f387280cd3030
ajz34 <17110220038@fudan.edu.cn>
2020-02-12 10:24:49 +0800

    cpp 0017

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -2,12 +2,13 @@
 // Created by ajz34 on 2020/2/9.
 //
 
-#include "e0001_e0100/e0016_three_sum_closest.h"
+#include "e0001_e0100/e0017_letter_comb.h"
+#include "main.h"
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    vector<int> v { -1, 2, 1, -4 };
-    cout << Solution().threeSumClosest(v, 1) << endl;
+    string s { "23" };
+    cout << Solution().letterCombinations(s) << endl;
 }
