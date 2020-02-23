0ed881c4bd6e4395cf6a2aff59f6d5aa8312994e
ajz34 <17110220038@fudan.edu.cn>
2020-02-23 19:11:31 +0800

    week 176 cpp 5170 additional test case

--- cpp/src/competition/week_177/e5170.cpp
+++ cpp/src/competition/week_177/e5170.cpp
@@ -14,14 +14,24 @@ public:
                 if (father[rightChild[i]] == -1) father[rightChild[i]] = i;
                 else return false;
         }
-        int root_count = 0;
-        for (auto f : father) {
-            if (f == -1) {
-                root_count += 1;
-                if (root_count >= 2) return false;
+        int root = -1;
+        for (int i = 0; i < father.size(); ++i) {
+            if (father[i] == -1) {
+                if (root != -1) return false;
+                root = i;
             }
         }
-        return root_count == 1;
+        if (root == -1) return false;
+
+        queue<int> q; q.push(root);
+        int qcnt = 1;
+        while (!q.empty()) {
+            int f = q.front();
+            q.pop();
+            if (leftChild[f] != -1) { q.push(leftChild[f]); ++qcnt; }
+            if (rightChild[f] != -1) { q.push(rightChild[f]); ++qcnt; }
+        }
+        return qcnt == n;
     }
 };
 
@@ -38,4 +48,12 @@ TEST(week177, e5170) {
     vl = str_to_vec<int>("[1,-1,-1,4,-1,-1]");
     vr = str_to_vec<int>("[2,-1,-1,5,-1,-1]");
     ASSERT_FALSE(Solution().validateBinaryTreeNodes(6, vl, vr));
+    // !!!IMPORTANT!!!
+    // test case from https://leetcode-cn.com/circle/discuss/8iTNwb/view/xaoqL8/
+    vl = str_to_vec<int>("[1,0,-1]");
+    vr = str_to_vec<int>("[-1,-1,-1]");
+    ASSERT_FALSE(Solution().validateBinaryTreeNodes(3, vl, vr));
+    vl = str_to_vec<int>("[-1, 2, 1]");
+    vr = str_to_vec<int>("[-1,-1,-1]");
+    ASSERT_FALSE(Solution().validateBinaryTreeNodes(3, vl, vr));
 }
