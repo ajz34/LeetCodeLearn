538b52cc1aacf041ea52316377419a802c538686
ajz34 <17110220038@fudan.edu.cn>
2020-03-03 13:17:19 +0800

    cpp lcci 10.01

---
+++ cpp/src/exercise/lcci/lcci1001.cpp
@@ -0,0 +1,28 @@
+// https://leetcode-cn.com/problems/sorted-merge-lcci/
+
+// 耗时：0:07:08
+// 错误次数：1
+// 错误类型：极端情况失误
+
+#include "extern.h"
+
+class Solution {
+public:
+    void merge(vector<int>& A, int m, vector<int>& B, int n) {
+        int pA = m - 1, pB = n - 1, p = m + n - 1;
+        while (p >= 0) {
+            if (pB < 0) A[p--] = A[pA--];
+            else if (pA < 0) A[p--] = B[pB--];
+            else if (pB < 0 || A[pA] > B[pB]) A[p--] = A[pA--];
+            else A[p--] = B[pB--];
+        }
+    }
+};
+
+TEST(lcci, lcci1001) {
+    vector<int> A = str_to_vec<int>("[1,2,3,0,0,0]");
+    vector<int> B = str_to_vec<int>("[2,5,6]");
+    vector<int> ans = str_to_vec<int>("[1,2,2,3,5,6]");
+    Solution().merge(A, 3, B, 3);
+    ASSERT_THAT(A, ans);
+}
